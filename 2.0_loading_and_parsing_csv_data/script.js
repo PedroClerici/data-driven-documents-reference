d54fd01e1278bfbe02e9124aa36a3893882de5ab
Pedro Clerici <pedro.clerici@tutamail.com>
2022-08-23 17:53:51 -0300

    Finished 2.0_loading_and_parsing_csv_data

---
+++ 2.0_loading_and_parsing_csv_data/script.js
@@ -0,0 +1,14 @@
+const url = 'https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv'
+
+// const fetchText = async () => {
+//   const response = await fetch(url);
+//   const text = await response.text();
+//   console.log(text);
+// }
+// fetchText()
+
+// Or
+
+fetch(url)
+  .then(Response => Response.text())
+  .then(text => console.log(text));
